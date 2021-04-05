48972b33eea222f5afb14ab3f6910b77884f4da5
Sven <glaglasven@live.com>
2021-04-05 12:44:14 +0200

    Update drawing

--- robot/models/robot.py
+++ robot/models/robot.py
@@ -4,10 +4,10 @@ from coordinates import Coordinate
 
 class Robot:
     def __init__(self):
-        self.J1 = Joint('B', _structure_offset=Coordinate(x=20/100, y=20/100))
-        self.J2 = Joint('B', _structure_offset=Coordinate(x=-20/100, y=20/100))
-        self.J3 = Joint('B', _structure_offset=Coordinate(x=20/100, y=-20/100))
-        self.J4 = Joint('B', _structure_offset=Coordinate(x=-20/100, y=-20/100))
+        self.J1 = Joint('B', _structure_offset=Coordinate(x=20/100, y=20/100), _invert_y=True)
+        self.J2 = Joint('A', _structure_offset=Coordinate(x=-20/100, y=20/100), _invert_y=True)
+        self.J3 = Joint('B', _structure_offset=Coordinate(x=-20/100, y=-20/100))
+        self.J4 = Joint('A', _structure_offset=Coordinate(x=20/100, y=-20/100))
 
     def update_position(self, x_i, forward):
         self.J1.update_position(x_i, forward)
@@ -20,6 +20,9 @@ class Robot:
         self.J2.draw(frame)
         self.J3.draw(frame)
         self.J4.draw(frame)
-    
+
     def draw_legs(self, frame):
         self.J1.draw_legs(frame)
+        self.J2.draw_legs(frame)
+        self.J3.draw_legs(frame)
+        self.J4.draw_legs(frame)
