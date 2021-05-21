3c125e2791e28416f3eaf4b20835a2b9cc10b83b
Sven <glaglasven@live.com>
2021-05-21 11:30:25 +0200

    Sequence I

--- robot/models/joint.py
+++ robot/models/joint.py
@@ -217,7 +217,6 @@ class Joint:
             if (position >= max_left) and (position < (max_left + self.d_top)):
                 self.move_mid_block(theta=-self.theta_s_bot)
                 self.move_top_block(position=position)
-
             if (position >= (max_left + self.d_top)) and (position <= max_right):
                 self.move_mid_block(position=position)
                 self.move_top_block(theta=self.theta_s_top)
@@ -388,7 +387,27 @@ class Joint:
         """
         00 -> 10 -> 01 -> 11 -> 01 -> 10 -> 00
         """
-        pass
+        position = u_i + self.x_offset
+
+        max_left = - (self.d_bot / 2) - (self.d_top / 2)
+        max_right = (self.d_bot / 2) + (self.d_top / 2)
+
+        if forward:
+            if (position >= max_left) and (position < (max_left + self.d_bot)):
+                self.move_mid_block(position=position)
+                self.move_top_block(theta=-self.theta_s_top)
+                # Would need a transition between this jump
+            if (position >= (max_left + self.d_top)) and (position <= max_right):
+                self.move_mid_block(position=position)
+                self.move_top_block(theta=self.theta_s_top)
+        else:
+            if (position <= max_right) and (position > (max_right - self.d_bot)):
+                self.move_mid_block(position=position)
+                self.move_top_block(theta=self.theta_s_top)
+                # Would need a transition between this jump
+            if (position <= (max_right - self.d_bot)) and (position >= max_left):
+                self.move_mid_block(position=position)
+                self.move_top_block(theta=-self.theta_s_top)
 
     def update_seq_J(self, u_i, forward):
         """
