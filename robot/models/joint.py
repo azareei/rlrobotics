1ce031bc506f5fe6241df410cb69e3f4e84b1062
Sven <glaglasven@live.com>
2021-06-14 14:33:35 +0200

    Sequencce J Update phase 0

--- robot/models/joint.py
+++ robot/models/joint.py
@@ -552,22 +552,41 @@ class Joint:
         max_right = (self.d_bot / 2) + (self.d_top / 2)
 
         if forward:
-            if (position >= max_left) and (position < (max_left + self.d_top)):
-                self.move_mid_block(theta=-self.theta_s_bot)
-                self.move_top_block(position=position)
-                # Would need a transition between this jump
-            if (position >= (max_left + self.d_bot)) and (position <= max_right):
-                self.move_mid_block(theta=self.theta_s_top)
-                self.move_top_block(position=position)
+            if self.invert_init_angle:
+                if (position >= (max_left + self.d_bot)) and (position <= max_right):
+                    self.move_mid_block(theta=self.theta_s_bot)
+                    self.move_top_block(position=position)
+                    # Would need a transition between this jump
+                if (position >= max_left) and (position < (max_left + self.d_top)):
+                    self.move_mid_block(theta=-self.theta_s_top)
+                    self.move_top_block(position=position)
+            else:
+                if (position >= max_left) and (position < (max_left + self.d_top)):
+                    self.move_mid_block(theta=-self.theta_s_bot)
+                    self.move_top_block(position=position)
+                    # Would need a transition between this jump
+                if (position >= (max_left + self.d_bot)) and (position <= max_right):
+                    self.move_mid_block(theta=self.theta_s_top)
+                    self.move_top_block(position=position)
         else:
-            if (position <= max_right) and (position > (max_right - self.d_bot)):
-                self.move_mid_block(position=position)
-                self.move_top_block(theta=self.theta_s_top)
-                # Would need a transition between this jump
-            if (position <= (max_right - self.d_bot)) and (position >= max_left):
-                self.move_top_block(theta=-self.theta_s_top)
-                self.move_mid_block(position=position)
-                self.move_top_block(theta=-self.theta_s_top)
+            if self.invert_init_angle:
+                if (position <= (max_right - self.d_bot)) and (position >= max_left):
+                    self.move_mid_block(theta=-self.theta_s_top)
+                    self.move_top_block(position=position)
+                    # Would need a transition between this jump
+                if (position <= max_right) and (position > (max_right - self.d_bot)):
+                    self.move_mid_block(theta=self.theta_s_top)
+                    self.move_top_block(position=position)
+                    self.move_mid_block(theta=self.theta_s_top)
+            else:
+                if (position <= max_right) and (position > (max_right - self.d_bot)):
+                    self.move_mid_block(theta=self.theta_s_top)
+                    self.move_top_block(position=position)
+                    # Would need a transition between this jump
+                if (position <= (max_right - self.d_bot)) and (position >= max_left):
+                    self.move_mid_block(theta=-self.theta_s_top)
+                    self.move_top_block(position=position)
+                    self.move_mid_block(theta=-self.theta_s_top)
 
     def update_seq_K(self, u_i, forward):
         """
