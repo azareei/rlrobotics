b7422f9f4d86e94fd9c1b4dd9b2419bfb2892cec
Sven <glaglasven@live.com>
2021-06-18 16:38:51 +0200

    Documentation for spring

--- robot/models/spring.py
+++ robot/models/spring.py
@@ -1,3 +1,26 @@
+"""
+Module spring
+
+This module represent a spring inside a Joint between two blocks
+
+Attributes
+----------
+P : Coordinates
+    Coordinates of the anchor on the bottom block for the  spring in Joint reference frame
+Q : Coordinates
+    Coordinates of the anchor on the top block for the  spring in Joint reference frame
+k : float
+    Stiffness of the spring
+l_0 : float
+    Initial length of the spring.
+
+Methods
+-------
+__init__(self, _P, _Q)
+    Initialize the spring
+draw(self, frame, offset, invert_y)
+    Draw the spring
+"""
 import cv2
 from utils import Utils
 
@@ -6,6 +29,13 @@ class Spring:
     def __init__(self, _P, _Q):
         """
         Define a spring between two blocks.
+
+        Parameters
+        ----------
+        _P : Coordinates
+            Coordinates of the anchor on the bottom block for the  spring in Joint reference frame
+        _Q : Coordinates
+            Coordinates of the anchor on the top block for the  spring in Joint reference frame
         """
         self.P = _P
         self.Q = _Q
@@ -13,10 +43,25 @@ class Spring:
         self.l_0 = 1/100
 
     def draw(self, frame, offset, invert_y):
-        if invert_y:
-            inv = -1
-        else:
-            inv = 1
+        """
+        Method responsible to draw the spring.
+
+        Parameters
+        ----------
+        frame : numpy Array
+            Image of the current frame to draw on.
+        offset : Coordinate
+            Offset of the arms to the robot coordinates
+        invert_y : bool
+            If true, means that we the structure is reverted in y axis.
+
+        Returns
+        -------
+        frame : numpy Array
+            Updated image with the spring on it.
+        """
+        inv = -1 if invert_y else 1
+        spring_thickness = 3
 
         return cv2.line(
             frame,
@@ -29,5 +74,5 @@ class Spring:
                 Utils.ConvertY(inv * (self.Q.y) + offset.y)
             ),
             (0, 100, 100),
-            thickness=3
+            thickness=spring_thickness
         )
